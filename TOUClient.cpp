d2a8479bb324aaa6ac8faf4fd8acec0025c9e4ad
Darkhan Kubigenov <darkhanu@gmail.com>
2013-11-16 16:37:45 -0600

    added init method into client

--- TOUClient.cpp
+++ TOUClient.cpp
@@ -1,6 +1,73 @@
 #include "TOUClient.h"
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-TOUClient::TOUClient() {
-	
+using namespace std;
+
+TOUClient::TOUClient(char *domainName, int port) {
+	this->domainName = domainName;
+	this->port = port;
+	this->currentState = NOT_INIT;
+}
+
+TOUClient::~TOUClient() {
+	// TODO
+	printf("Inside destructor\n");
+}
+/**
+ * Initialize the client
+ */
+int TOUClient::init() {
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM;
+	const char * strPort = to_string(this->port).c_str();
+
+	if ((rv = getaddrinfo(this->domainName, strPort, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and make a socket
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to bind socket\n");
+		return 2;
+	}
+
+	this->currentState = CLOSED;
+	// return with no error
+	return 0;
+}
+
+int TOUClient::connect() {
+	if (this->currentState == NOT_INIT) {
+		perror("connect(): the client is not initialized");
+		return 1;
+	} else if (this->currentState != CLOSED) {
+		perror("connect(): the client is not in CLOSED state");
+		return 1;
+	}
+
+	return 0;
 }
--- TOUClient.h
+++ TOUClient.h
@@ -1,11 +1,34 @@
 #ifndef TOUCLIENT_H
 #define TOUCLIENT_H
 
+enum CLIENT_STATE {NOT_INIT, CLOSED, SYN_SENT, ESTABLISHED, FIN_WAIT_1,
+					FIN_WAIT_2, TIME_WAIT};
+
 class TOUClient {
 private:
-	
+	char *domainName;
+	int port;
+	int sockfd;
+	CLIENT_STATE currentState;
 public:
-	TOUClient();
+	/**
+	 * Constructor
+	 * domainName: domain name to which client will connect
+	 * port: port number to which to connect
+	 */
+	TOUClient(char *domainName, int port);
+	~TOUClient();
+
+	/*
+	 * Initialize the client
+	 * return 0 on success, and nonzero otherwise
+	 */
+	int init();
+	/*
+	 * Connect to server
+	 * return 0 on success, and nonzero otherwise
+	 */
+	int connect();
 };
 
 #endif
